2f6dae9a60a63811815a0a8808fb72fe1f44305c
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-23 19:50:32 +0700

    create collection

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ mongoose.connect('mongodb://localhost/mean0705')
 .then(() => console.log('Database connected'))
 .catch(error => console.log('Cannot connect database', error));
 
+const singerSchema = new mongoose.Schema({
+    name: { type: String, require: true, trim: true },
+    link: { type: String, require: true, unique: true, trim: true },
+    image: { type: String, required: true, unique: true, trim: true }
+});
+
+const Singer = mongoose.model('Singer', singerSchema);
+
 /*
     Connect
     Tao 1 cai collection
