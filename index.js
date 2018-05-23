865af8cd441e760c4eaeea1fab32862313a6c3b8
Van Pho Nguyen <vanpho02@gmail.com>
2018-05-23 20:07:41 +0700

    insert man

--- index.js
+++ index.js
@@ -12,9 +12,29 @@ const singerSchema = new mongoose.Schema({
 
 const Singer = mongoose.model('Singer', singerSchema);
 
+Singer.find({})
+.then(singers => console.log(singers));
+
+// const singer = new Singer({
+//     name: 'Teo',
+//     link: 'http://fb.com/teo',
+//     image: 'http://fb.com/teo.png'
+// });
+// singer.save()
+// .then(s => console.log(s))
+// .catch(error => console.log(error));
+
+// Singer.insertMany([
+//     { name: 'Karik', link: 'https://mp3.zing.vn/nghe-si/Karik', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/a/0/a0927398989d4c5b18c56880bd56442b_1509531352.jpg' },
+//     { name: 'Đức Phúc', link: 'https://mp3.zing.vn/nghe-si/Duc-Phuc', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/d/7/d7f34aa6b1112e4b605f6c6e7eccd162_1509437674.jpg' },
+//     { name: 'Châu Khải Phong', link: 'https://mp3.zing.vn/nghe-si/Chau-Khai-Phong', image: 'https://zmp3-photo.zadn.vn/thumb/240_240/avatars/c/a/ca59799621e1c9fd8652cd947713acfa_1509951552.jpg' },
+// ])
+// .then(x => console.log(x))
+// .catch(error => console.log(error));
+
 /*
-    Connect
-    Tao 1 cai collection
+    Connect √
+    Tao 1 cai collection √
     Tao documents
     Select
     Insert
